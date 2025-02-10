407bdf236f0fdf4df777a081272fa828ec42ce6c
Tushar Ahuja <93457512+Tusharahuja14@users.noreply.github.com>
2025-02-10 20:41:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const size=document.getElementById("sizeInfo");
+function update()
+{
+size.innerText=`Width:${window.innerWidth} and Height:${window.innerHeight}`;
+}
+update();
+//window.addEventListener("resize",update);
